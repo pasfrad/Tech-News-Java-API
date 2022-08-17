26d0c888bf6a81bfbe947258cb1550e2d04076bd
Reese <rd.hart@example.com>
2022-08-17 13:54:06 -0400

    Hello World and variable practice

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,13 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args){
+        int six = 6;
+        double twelveish = 12.33;
+        double sum = six + twelveish;
+        String color = "green";
+        String concat = six + color;
+        System.out.println(sum);
+        System.out.println(concat);
+    }
+}
