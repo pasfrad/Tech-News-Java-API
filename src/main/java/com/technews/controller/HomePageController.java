9e6b9e286ee3d246772afd35b3d68722c31ee56e
Reese <rd.hart@outlook.com>
2022-09-26 11:39:33 -0400

    add TechNewsController; still need to debug

--- src/main/java/com/technews/controller/HomePageController.java
+++ src/main/java/com/technews/controller/HomePageController.java
@@ -2,12 +2,14 @@ package com.technews.controller;
 
 import com.technews.model.Post;
 import com.technews.model.User;
+import com.technews.model.Comment;
 import com.technews.repository.CommentRepository;
 import com.technews.repository.PostRepository;
 import com.technews.repository.UserRepository;
 import com.technews.repository.VoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
